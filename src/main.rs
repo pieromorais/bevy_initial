f728a71ff23711f19114f696ee6d908d8fc61996
Piero Morais <piero.morais@gmail.com>
2024-06-15 12:06:08 -0300

    Finalize starting tutotial for Bevy
    Completed all steps to create a simple APP using Bevy
    Creation of few entities Person, Name.
    Creation of few system greeting function that says hello for every
    person with name
    Create a resource to modularize our code
    Implement a timer with Time resource

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (update_people, greet_people).chain());
+    }
 }
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, HelloPlugin))
+        .run();
+}
+
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break;
+        }
+    }
+}
+
+
